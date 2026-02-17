03cd29028408ffc8bd5a54c3d87c96c159deb1c1
Sri Mohan <167277292+sri-mohan-abburi@users.noreply.github.com>
2026-02-16 23:29:26 -0600

    Sec: Added JWT generation logic to security module

--- app/core/security.py
+++ app/core/security.py
@@ -1,4 +1,8 @@
 from passlib.context import CryptContext
+from datetime import datetime, timedelta, timezone
+from typing import Any, Union
+from jose import jwt
+from app.core.config import settings
 
 # Bcrypt is a strong hashing algorithm that is widely used for password hashing.
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -12,3 +16,27 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
 def get_password_hash(password: str) -> str:
     """Hash a password using bcrypt."""
     return pwd_context.hash(password)
+
+
+def create_access_token(
+    subject: Union[str, Any], expires_delta: timedelta = None
+) -> str:
+    """
+    Generates a JWT Token.
+    payload: The data inside the token (like user_id).
+    secret: The 'digital signature' key from .env.
+    """
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(
+            minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
+        )
+
+    to_encode = {"exp": expire, "sub": str(subject)}
+
+    # This creates the encrypted string
+    encoded_jwt = jwt.encode(
+        to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM
+    )
+    return encoded_jwt
