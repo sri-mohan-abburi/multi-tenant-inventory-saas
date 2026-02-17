d2cbb77822b2b666887e5f7519163564b72647d7
Sri Mohan <167277292+sri-mohan-abburi@users.noreply.github.com>
2026-02-16 23:30:43 -0600

    Feat: Implemented OAuth2 Login endpoint to issue JWTs

---
+++ app/api/v1/endpoints/login.py
@@ -0,0 +1,42 @@
+from datetime import timedelta
+from typing import Any
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.api import deps
+from app.core import security
+from app.core.config import settings
+from app.crud import crud_user
+
+router = APIRouter()
+
+
+@router.post("/access-token")
+async def login_access_token(
+    db: AsyncSession = Depends(deps.get_db),
+    form_data: OAuth2PasswordRequestForm = Depends(),
+) -> Any:
+    """
+    OAuth2 compatible token login, get an access token for future requests.
+    """
+    # 1. Find the user by Email
+    user = await crud_user.get_user_by_email(db, email=form_data.username)
+
+    # 2. Verify Password
+    if not user or not security.verify_password(
+        form_data.password, user.hashed_password
+    ):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+        )
+
+    # 3. Create Token
+    access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    return {
+        "access_token": security.create_access_token(
+            subject=user.id, expires_delta=access_token_expires
+        ),
+        "token_type": "bearer",
+    }
